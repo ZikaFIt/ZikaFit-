46a3c7321a0dc3cfa309c1ff4ed9021d87b25963
ZikaFIt <moha20034567@gmail.com>
2025-09-07 15:15:30 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ZikaFit-/',   // ← لازم يبقى نفس اسم الريبو بالظبط (عندك فيه شرطة في الآخر)
+  base: '/ZikaFit-/',   // لازم اسم الريبو بالظبط مع الشرطة
 })
